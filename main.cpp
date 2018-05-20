4136a1efcbf4c697d1f716c9adf64af1aa57f4fc
azhar <azhartahir0100@gmail.com>
2018-05-20 18:38:49 +0500

    Added map

--- main.cpp
+++ main.cpp
@@ -2,17 +2,24 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <math.h>
+#include "SFMLOrthogonalLayer.hpp"
+#include <tmxlite/Map.hpp>
 
-
-double approximate_to_quadrantal(double angle){
-	*std::min_element({90-angle,180-angle,270-angle,360-angle});
+// enum{
+// 	KEY_UP,KEY_DOWN,KEY_RIGHT,KEY_LEFT
+// }
+// double approximate_to_quadrantal(double angle){
+// 	return *std::min_element({90-angle,180-angle,270-angle,360-angle});
+// }
+bool is_perpendicular(const sf::Vector2f& a,const sf::Vector2f& b){
+	return a.x*b.x + a.y*b.y == 0;
 }
 
-sf::Vector2f normalize(const sf::Vector2f& source)
+sf::Vector2f normalize(const sf::Vector2f& source,double magnitude=1)
 {
     float length = sqrt((source.x * source.x) + (source.y * source.y));
     if (length != 0)
-        return sf::Vector2f(source.x / length, source.y / length);
+        return sf::Vector2f(source.x * magnitude / length, source.y * magnitude / length);
     else
         return source;
 }
@@ -26,13 +33,13 @@ public:
 
 	Player(){
 
-// resize it to 5 points
-	face.setPointCount(3);
+		// resize it to 5 points
+		face.setPointCount(3);
 
-	// define the points
-	face.setPoint(0, sf::Vector2f(0, 0));
-	face.setPoint(1, sf::Vector2f(20, 20));
-	face.setPoint(2, sf::Vector2f(0, 40));
+		// define the points
+		face.setPoint(0, sf::Vector2f(0, 0));
+		face.setPoint(1, sf::Vector2f(20, 20));
+		face.setPoint(2, sf::Vector2f(0, 40));
 
 		body = sf::CircleShape(20);
 		face.setPosition(getPosition() + sf::Vector2f({40,0}));
@@ -76,18 +83,26 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML shapes", sf::Style::Default, settings);
 
 
+	bool keys[4] = {0,1,2,3};
     // run the program as long as the window is open
     window.setFramerateLimit(60);
 	sf::Clock clock;
     sf::Time time;
 
-    sf::Vector2f movement;
-    // sf::CircleShape player(20);
+    // sf::Vector2f movement;
+    // sf::CircleShape player();
     // bool draw_sword = false;
     Player p;
     Player z;
     // sf::RectangleShape sword(sf::Vector2f({40,5}));
     // sword.setFillColor(sf::Color(255,0,0));
+
+	tmx::Map map;
+    map.load("new/untitled.tmx");
+
+    MapLayer layerZero(map, 0);
+	MapLayer layerOne(map, 1);
+	MapLayer layerTwo(map, 2);
 
     while (window.isOpen())
     {
@@ -96,6 +111,8 @@ int main()
         time = clock.getElapsedTime();
         std::cout << 1.0f/time.asSeconds() << std::endl;
 
+        sf::Vector2f movement;
+        sf::Vector2f input;
         while (window.pollEvent(event))
         {
 
@@ -105,26 +122,38 @@ int main()
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
 			{
+				input = sf::Vector2f({5,0});
 			    // left key is pressed: move our character
-			    p.move(5, 0);
+			    if (is_perpendicular(movement,input)){
+			    	movement += input;
+			    }
 			    p.turnto(sf::Vector2f({5,0}));
 			}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
 			{
 			    // left key is pressed: move our character
-			    p.move(-5, 0);
+			    input = sf::Vector2f(-5, 0);
+			    if (is_perpendicular(movement,input)){
+			    	movement += input;
+			    }
 			    p.turnto(sf::Vector2f({-5,0}));
 			}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
 			{
 			    // left key is pressed: move our character
-			    p.move(0, 5);
+			    input = sf::Vector2f(0, 5);
+			    if (is_perpendicular(movement,input)){
+			    	movement += input;
+			    }
 				p.turnto(sf::Vector2f({0,5}));
 			}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
 			{
 			    // left key is pressed: move our character
-			    p.move(0, -5);
+			    input = sf::Vector2f(0, -5);
+				if (is_perpendicular(movement,input)){
+			    	movement += input;
+			    }
 			    p.turnto(sf::Vector2f({0,-5}));
 			}
 
@@ -143,12 +172,16 @@ int main()
 
 
         }
+        p.move(normalize(movement,5));
 
 			sf::Vector2f del = p.getPosition() - z.getPosition();
 			z.move(normalize(del));
 			z.turnto(del);
 
 	    window.clear(sf::Color());
+        window.draw(layerZero);
+        window.draw(layerOne);
+		window.draw(layerTwo);
 	    window.draw(p);
 	    window.draw(z);
 	    // if(draw_sword)
